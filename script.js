4ff0349051c3ecbb3e9cf726fcb2e380ccae7733
Barsbek <95858963+DaimenGGer@users.noreply.github.com>
2022-12-21 02:36:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//Задание-1
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+//Задание-2
+for (let i = 100; i >= 1; i--) {
+    console.log(i);
+}
+//Задание-3
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
+//Задание-4
+let arr = [];
+for (let i = 0; i < 10; i++) {
+  arr.push("x");
+}
+console.log(arr);
+//Задание-5
+let arr2 = [];
+for (let i = 1; i <= 10; i++) {
+  arr2.push(i);
+}
+console.log(arr2);
